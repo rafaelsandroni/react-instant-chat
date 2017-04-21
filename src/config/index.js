d0a0bac01b75f657d0b6f53dffbd910b24448a03
Rafael Sandroni <rafaelsandroni@users.noreply.github.com>
2017-04-21 12:04:22 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,5 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
-  api: 'https://lumrffawpm.localtunnel.me'
+  api: 'https://snzbkfuoqn.localtunnel.me'
 }
